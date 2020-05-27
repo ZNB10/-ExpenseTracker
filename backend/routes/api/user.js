9756af35c1460bbb2ef9630ffe3a7c84dee8e2b9
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-26 21:35:11 -0600

    Securoty-P2

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,2 +1,41 @@
 var ObjectID =require('mongodb').ObjectID;
 var bcrypt = require('bcrypt');
+
+module.exports = function(db){
+    var userColl = db.collection('users');
+    var userModel = {}
+    
+    //Obtener usuario por correo
+    userModel.getByEmail = function(email, handler){
+        var query = {"email": email};
+        userColl.findOne(query, (err, user)=>{
+            if(err){
+                console.log(err);
+                return handler(err, null);
+            }
+            if(!user){
+                return handler(new Error("No se encontro el usuario"), null);
+            }
+            return handler(null, user);
+        });
+    }
+
+    //Ingreso de nuevo user
+    userModel.addNew = (email, password, handler) =>{
+        var newUser = Object.assign({}, {email:email, password:genPassword() });
+        userColl.insertOne(newUser, (err, result)=>{
+            if(err){
+                console.log(err);
+                return handler(err, null);
+            }
+            if(){
+                
+            }
+        });
+    };//addNewUser
+
+    function genPassword(rawPassword){
+        return rawPassword;
+    }
+    return userModel;
+}
