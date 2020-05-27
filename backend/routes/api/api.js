c3289b3609d88f5e2f7ab7631e071248b876dda5
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-05-27 02:14:52 -0600

    Validate JWT

--- backend/routes/api/api.js
+++ backend/routes/api/api.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const ExtractJWT = passportJWT.ExtractJwt;
+const jwtStrategy = pportJWT.Strategy;
 
-function routerInit(db){
+
+function routerInit(db) {
+
+    pport.use(
+        new jwtStrategy({
+                jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToekn(),
+                secretOrKey: "acklendavenuechallenge"
+            },
+            (payload, next) => {
+                var user = payload;
+                return next(null, user);
+            }
+        )
+    );
+
     const securityApi = require('./security')(db);
     const thingsApi = require('./things')(db);
 
-    router.get('/', (req, res, next)=>{
-        res.status(200).json({"ok":"Api version 1"});
+    router.get('/', (req, res, next) => {
+        res.status(200).json({ "ok": "Api version 1" });
     });
 
     router.use('/security', securityApi);
