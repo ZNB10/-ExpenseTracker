981c8e745d9a0e57cf3ab1173c21d5620420a80d
Yonathan Cruz <yonathancruz2019@gmail.com>
2020-06-01 04:05:49 -0600

    Configurando ambiente de produccion

---
+++ backend/public/precache-manifest.7eb01a8d487377cbe537913a9d46be37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26415c6b07d2e45e5df94941536ad8eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26bee62cc960c61d5dfb",
+    "url": "/static/css/main.242420aa.chunk.css"
+  },
+  {
+    "revision": "34f4673d4aed47d2034a",
+    "url": "/static/js/2.5adb6349.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5adb6349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26bee62cc960c61d5dfb",
+    "url": "/static/js/main.999235c5.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
